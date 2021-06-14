dd9743d236400bfc488dfabaccddedfdc8a777e5
jakob <jakob.germano@gmail.com>
2021-06-14 17:47:50 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ function updateEmployeeWithKeyAndValue(employee, key, value) {
     return Object.assign({}, employee, { [key]: value}) 
 }
 
+
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
 
     employee[key] = value
     return employee
 }
+
 
 function deleteFromEmployeeByKey(employee, key) {
     const newObject = Object.assign({}, employee)
